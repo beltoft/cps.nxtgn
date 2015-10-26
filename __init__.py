a4183b5c549e49e13a27ff6708b4142cebdf91be
beltoft <mbeltoft@gmail.com>
2015-10-26 21:20:59 +0100

    Corrected link name

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ config = [{
 			'tab': 'searcher',
 			'list': 'torrent_providers',
 			'name': 'NxtGn',
-			'description': 'See <a href="http://nxtgn.biz">NxGn</a>',
+			'description': 'See <a href="http://nxtgn.biz">NxtGn</a>',
 			'wizard': True,
 			'options': [
 				{
